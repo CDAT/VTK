406eca767430ede383577f8e7a435bbe36758531
Will Schroeder <will.schroeder@kitware.com>
2016-11-01 07:46:19 -0400

    Use loop walking, more testing

--- Filters/Modeling/Testing/Python/TestCookieCutter.py
+++ Filters/Modeling/Testing/Python/TestCookieCutter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import vtk
 from vtk.util.misc import vtkGetDataRoot
 
@@ -44,6 +43,7 @@ glyphPts.InsertPoint(9,  0.0, 0.75,0.0)
 glyphPts.InsertPoint(10, -0.5,0.0,0.0)
 glyphPts.InsertPoint(11, -0.75,0.0,0.0)
 glyphPts.InsertPoint(12, 0.0,0.0,0.0)
+glyphPts.InsertPoint(13, -0.9,0.0,0.0)
 
 glyphVerts.InsertNextCell(1)
 glyphVerts.InsertCellPoint(12)
@@ -57,9 +57,11 @@ glyphLines.InsertCellPoint(7)
 glyphLines.InsertNextCell(2)
 glyphLines.InsertCellPoint(8)
 glyphLines.InsertCellPoint(9)
-glyphLines.InsertNextCell(2)
+# The last line is a polyline
+glyphLines.InsertNextCell(3)
 glyphLines.InsertCellPoint(10)
 glyphLines.InsertCellPoint(11)
+glyphLines.InsertCellPoint(13)
 
 glyphPolys.InsertNextCell(4)
 glyphPolys.InsertCellPoint(0)
@@ -72,37 +74,73 @@ glyph.SetInputConnection(plane.GetOutputPort())
 glyph.SetSourceData(glyphData)
 glyph.SetScaleFactor( 0.02 )
 
-# Create a loop for cookie cutting
-loop = vtk.vtkPoints()
-loop.SetNumberOfPoints(4)
-loop.SetPoint(0, -0.35,0.0,0.0)
-loop.SetPoint(1, 0,-0.35,0.0)
-loop.SetPoint(2, 0.35,0.0,0.0)
-loop.SetPoint(3, 0.0,0.35,0.0)
+# Create multiple loops for cookie cutting
+loops = vtk.vtkPolyData()
+loopPts = vtk.vtkPoints()
+loopPolys = vtk.vtkCellArray()
+loops.SetPoints(loopPts)
+loops.SetPolys(loopPolys)
 
-cookie = vtk.vtkCookieCutFilter()
+loopPts.SetNumberOfPoints(16)
+loopPts.SetPoint(0, -0.35,0.0,0.0)
+loopPts.SetPoint(1, 0,-0.35,0.0)
+loopPts.SetPoint(2, 0.35,0.0,0.0)
+loopPts.SetPoint(3, 0.0,0.35,0.0)
+
+loopPts.SetPoint(4, -0.35,-0.10,0.0)
+loopPts.SetPoint(5, -0.35,-0.35,0.0)
+loopPts.SetPoint(6, -0.10,-0.35,0.0)
+
+loopPts.SetPoint(7, 0.35,-0.10,0.0)
+loopPts.SetPoint(8, 0.35,-0.35,0.0)
+loopPts.SetPoint(9, 0.10,-0.35,0.0)
+
+loopPts.SetPoint(10, 0.35,0.10,0.0)
+loopPts.SetPoint(11, 0.35,0.35,0.0)
+loopPts.SetPoint(12, 0.10,0.35,0.0)
+
+loopPts.SetPoint(13, -0.35,0.10,0.0)
+loopPts.SetPoint(14, -0.35,0.35,0.0)
+loopPts.SetPoint(15, -0.10,0.35,0.0)
+
+loopPolys.InsertNextCell(4)
+loopPolys.InsertCellPoint(0)
+loopPolys.InsertCellPoint(1)
+loopPolys.InsertCellPoint(2)
+loopPolys.InsertCellPoint(3)
+
+loopPolys.InsertNextCell(3)
+loopPolys.InsertCellPoint(4)
+loopPolys.InsertCellPoint(5)
+loopPolys.InsertCellPoint(6)
+
+loopPolys.InsertNextCell(3)
+loopPolys.InsertCellPoint(7)
+loopPolys.InsertCellPoint(8)
+loopPolys.InsertCellPoint(9)
+
+loopPolys.InsertNextCell(3)
+loopPolys.InsertCellPoint(10)
+loopPolys.InsertCellPoint(11)
+loopPolys.InsertCellPoint(12)
+
+loopPolys.InsertNextCell(3)
+loopPolys.InsertCellPoint(13)
+loopPolys.InsertCellPoint(14)
+loopPolys.InsertCellPoint(15)
+
+cookie = vtk.vtkCookieCutter()
 cookie.SetInputConnection(glyph.GetOutputPort())
-cookie.SetLoop(loop)
+cookie.SetLoopsData(loops)
 
 mapper = vtk.vtkPolyDataMapper()
-#mapper.SetInputConnection(glyph.GetOutputPort())
 mapper.SetInputConnection(cookie.GetOutputPort())
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
 
-loopData = vtk.vtkPolyData()
-loopPoly = vtk.vtkCellArray()
-loopPoly.InsertNextCell(4)
-loopPoly.InsertCellPoint(0)
-loopPoly.InsertCellPoint(1)
-loopPoly.InsertCellPoint(2)
-loopPoly.InsertCellPoint(3)
-loopData.SetPoints(loop)
-loopData.SetPolys(loopPoly)
-
 loopMapper = vtk.vtkPolyDataMapper()
-loopMapper.SetInputData(loopData)
+loopMapper.SetInputData(loops)
 
 loopActor = vtk.vtkActor()
 loopActor.SetMapper(loopMapper)
